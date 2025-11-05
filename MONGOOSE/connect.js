0e8087280664f7ab544e47e728948f425d3fcf36
RajatSaini04 <rajatsaini0411@gmail.com>
2025-11-05 22:48:46 +0530

    Updated Mongodb routes and created users

--- MONGOOSE/connect.js
+++ MONGOOSE/connect.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-
+const users = require('./MOCK_DATA.json');
 
 mongoose.connect('mongodb://127.0.0.1:27017/DEMO_DAY')
     .then(() => {
@@ -29,26 +29,38 @@ app.use(express.urlencoded({ extended: true }))
 
 // === REST API
 // GET -  ALL USER -using browser ye HTML dega - 
-app.get('/users', (req, res) => {
-    res.json(users)
+app.get('/users', async (req, res) => {
+
+    // getting user from the DB we Created
+    const allDbUsers = await User.find({});
+    res.json(allDbUsers)
 })
 
+app.route('/api/users/:id').get(
+    async (req, res) => {
+        const user = await User.findById(req.params.id);
+
+        if (!user) return res.status(404).json({ status: 'PEnding' })
+        return res.json(user)
+
+    }
+)
+
 //  POST - 
-app.post('/api/users', (req, res) => {
+app.post('/api/users', async (req, res) => {
     const body = req.body;
 
     if (!body.firstName || !body.email) {
         return res.status(400).json({ msg: 'ALL feild req..' })
     }
-
-    const result = User.create({
+    // Creating a user with help of modal 
+    const result = await User.create({
         firstName: body.firstName,
         email: body.email,
     });
-
+    console.log(result);
     return res.status(201).json({ msg: "success" })
 
 })
-
 
 app.listen(8000, () => { console.log('Server Started'); })
